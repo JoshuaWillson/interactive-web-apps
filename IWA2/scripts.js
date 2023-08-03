69ad54096a822d5e26ea179fe47afe67d4dc1bac
Joshua Willson <joshuawillsonmj@gmail.com>
2023-08-03 14:35:41 +0200

    reorganised IWA_02 scripts with added broken code

--- IWA2/scripts.js
+++ IWA2/scripts.js
@@ -1,5 +1,19 @@
 // IWA_02 Challenge 1
 
+/****************************************************************************/
+//Broken
+/* This is the firstname of the user /*
+
+const user = 'John'
+
+
+/* This is the lastname of the user /* const surname = 'Smith'
+
+
+console.log(user; surname;)
+
+/****************************************************************************/
+//Fixed
 /* This is the firstname of the user */
 
 const user = 'John'
@@ -12,8 +26,27 @@ const surname = 'Smith'
 
 console.log(user, surname)
 
+
+
+
+
+
+
 // IWA_02 Challenge 2
 
+/****************************************************************************/
+//Broken
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+//const date = '10/07/2014'
+
+//console(date)
+
+/****************************************************************************/
+//Fixed
 /** (c) ACME Inc. 2010 */
 
 /**
@@ -23,8 +56,34 @@ const date = '10/07/2014'
 
 console.log(date)
 
+
+
+
+
+
+
+
+
+
 // IWA_02 Challenge 3
 
+/****************************************************************************/
+//Broken
+// 
+//It is important to show the following message in order to:
+//- to assure users
+//- to scare hackers
+//- to impress investors
+//
+
+//console.warn(Security scan starting)
+
+/* It is important to let user know when they can close the page  /*
+
+cnosole.info('Please wait for scan to complete before closing the browser.)
+
+/****************************************************************************/
+//Fixed
 /* 
 It is important to show the following message in order to:
 - to assure users
