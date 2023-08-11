81b707383d179d1d4d70fe569f321c1d1f9ebbd2
Joshua Willson <joshuawillsonmj@gmail.com>
2023-08-11 13:44:34 +0200

    completed IWA_07 challenges

---
+++ IWA7/scripts.js
@@ -0,0 +1,35 @@
+// IWA_07
+
+// Challenge 1
+
+const value = 3 //"3" is a string which needs to be the number 3
+console.log(value + 4 + value) //value was a string therefore it was being concatinated instead of added
+
+// Challenge 2
+
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+console.log(`Good Morning, ${nickname || firstname}!`)
+
+// Challenge 3
+
+const leoName = 'Leo'
+const leoSurname = 'Musvaire     '
+const leoBalance = '-9394'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.2'
+
+const divider = '----------------------------------'
+
+// Only change below this line
+
+const owed = `R ${Math.abs(parseFloat(leoBalance) + parseFloat(sarahBalance)).toFixed(2)}`
+const leo = `${leoName} ${leoSurname.trimEnd()} (Owed: R ${Math.abs(parseFloat(leoBalance)).toFixed(2)})`
+const sarah = `${sarahName.trimEnd()} ${sarahSurname} (Owed: R ${Math.abs(parseFloat(sarahBalance)).toFixed(2)})`
+const total = "Total amount owed: "
+const result = `\n${leo} \n${sarah} \n\n${divider} \n  ${total} ${owed} \n${divider}`
+
+console.log(result)
