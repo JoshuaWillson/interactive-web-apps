4c1394b5499c85dcfe75634dc64bbb0360a520e6
Joshua Willson <joshuawillsonmj@gmail.com>
2023-08-17 18:27:59 +0200

    added follow along code to IWA_01 for IWA_12

--- IWA1/scripts.js
+++ IWA1/scripts.js
@@ -1,40 +1,59 @@
-const MAX_NUMBER = 14
-const MIN_NUMBER = -5
-const STEP_AMOUNT = 5
+const MAX_NUMBER = 15
+const MIN_NUMBER = -15
+const STEP_AMOUNT = 1
 
-const number = document.querySelector('[data-key = "number"]')
-const subtract = document.querySelector('[data-key = "subtract"]')
-const add = document.querySelector('[data-key = "add"]')
+const elements = {
+    number: document.querySelector('[data-key = "number"]'),
+    subtract:  document.querySelector('[data-key = "subtract"]'),
+    add:  document.querySelector('[data-key = "add"]'),
+
+}
+
+const updateColor = () => {
+    const value = elements.number.value
+    const singleStep = 250 / (MAX_NUMBER - MIN_NUMBER)
+    const distMax = MAX_NUMBER - value
+    const distMin = value - MIN_NUMBER
+    const red = distMax * singleStep
+    const green = distMin * singleStep
+    elements.number.style.color = `rgb(${red}, ${green}, 50)`;
+    
+}
 
 const subtractHandler = () => {
-    const newValue = parseInt(number.value) - STEP_AMOUNT
-    number.value = newValue
+    const newValue = parseInt(elements.number.value) - STEP_AMOUNT
+    elements.number.value = newValue
 
     if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true
+        elements.subtract.disabled = true
     
     }
 
-    if (add.disabled === true) {
-        add.disabled = false
+    if (elements.add.disabled === true) {
+        elements.add.disabled = false
     }
+
+    updateColor()
 
 }
 const addHandler = () => {
-    const newValue = parseInt(number.value) + STEP_AMOUNT
-    number.value = newValue
+    const newValue = parseInt(elements.number.value) + STEP_AMOUNT
+    elements.number.value = newValue
 
     if (newValue >= MAX_NUMBER) {
-        add.disabled = true
+        elements.add.disabled = true
     
     }
 
-    if (subtract.disabled === true) {
-        subtract.disabled = false
+    if (elements.subtract.disabled === true) {
+        elements.subtract.disabled = false
     }
+
+    updateColor()
 
 }
 
-subtract.addEventListener('click', subtractHandler)
-add.addEventListener('click', addHandler)
+elements.subtract.addEventListener('click', subtractHandler)
+elements.add.addEventListener('click', addHandler)
 
+updateColor()
