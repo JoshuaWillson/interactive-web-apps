d6d8fda1d9e587772390ed4d2b93a555517fbfdb
Joshua Willson <joshuawillsonmj@gmail.com>
2023-08-22 14:21:38 +0200

    improved IWA_11 by making use of objects

--- IWA11/scripts.js
+++ IWA11/scripts.js
@@ -1,32 +1,38 @@
-const root1 = document.querySelector('[data-key="order1"]')
-let biscuits1 = document.querySelector('[data-key="order1"] .biscuits .count')
-let donuts1 = document.querySelector('[data-key="order1"] .donuts .count')
-let pancakes1 = document.querySelector('[data-key="order1"] .pancakes .count')
-let status1 = document.querySelector('[data-key="order1"] .status dd')
+const roots = {
+    root1: {
+        dataKey: document.querySelector('[data-key="order1"]'),
+        biscuits: document.querySelector('[data-key="order1"] .biscuits .count'),
+        donuts: document.querySelector('[data-key="order1"] .donuts .count'),
+        pancakes: document.querySelector('[data-key="order1"] .pancakes .count'),
+        status: document.querySelector('[data-key="order1"] .status dd'),
+    },
+    root2: {
+        dataKey: document.querySelector('[data-key="order2"]'),
+        biscuits: document.querySelector('[data-key="order2"] .biscuits .count'),
+        donuts: document.querySelector('[data-key="order2"] .donuts .count'),
+        pancakes: document.querySelector('[data-key="order2"] .pancakes .count'),
+        status: document.querySelector('[data-key="order2"] .status dd'),
+    },
+    root3: {
+        dataKey: document.querySelector('[data-key="order3"]'),
+        biscuits: document.querySelector('[data-key="order3"] .biscuits .count'),
+        donuts: document.querySelector('[data-key="order3"] .donuts .count'),
+        pancakes: document.querySelector('[data-key="order3"] .pancakes .count'),
+        status: document.querySelector('[data-key="order3"] .status dd'),
+    },
+}
 
-const root2 = document.querySelector('[data-key="order2"]')
-let biscuits2 = document.querySelector('[data-key="order2"] .biscuits .count')
-let donuts2 = document.querySelector('[data-key="order2"] .donuts .count')
-let pancakes2 = document.querySelector('[data-key="order2"] .pancakes .count')
-let status2 = document.querySelector('[data-key="order2"] .status dd')
+roots.root1.biscuits.innerHTML = roots.root1.dataKey.getAttribute('data-biscuits')
+roots.root1.donuts.innerHTML = roots.root1.dataKey.getAttribute('data-donuts')
+roots.root1.pancakes.innerHTML = roots.root1.dataKey.getAttribute('data-pancakes')
+roots.root1.status.innerHTML = roots.root1.dataKey.getAttribute('data-delivered') === "true" ? 'Delivered' : 'Pending'
 
-const root3 = document.querySelector('[data-key="order3"]')
-let biscuits3 = document.querySelector('[data-key="order3"] .biscuits .count')
-let donuts3 = document.querySelector('[data-key="order3"] .donuts .count')
-let pancakes3 = document.querySelector('[data-key="order3"] .pancakes .count')
-let status3 = document.querySelector('[data-key="order3"] .status dd')
+roots.root2.biscuits.innerHTML = roots.root2.dataKey.getAttribute('data-biscuits')
+roots.root2.donuts.innerHTML = roots.root2.dataKey.getAttribute('data-donuts')
+roots.root2.pancakes.innerHTML = roots.root2.dataKey.getAttribute('data-pancakes')
+roots.root2.status.innerHTML = roots.root2.dataKey.getAttribute('data-delivered') === "true" ? 'Delivered' : 'Pending'
 
-biscuits1.innerHTML = root1.getAttribute('data-biscuits')
-donuts1.innerHTML = root1.getAttribute('data-donuts')
-pancakes1.innerHTML = root1.getAttribute('data-pancakes')
-status1.innerHTML = root1.getAttribute('data-delivered') === "true" ? 'Delivered' : 'Pending'
-
-biscuits2.innerHTML = root2.getAttribute('data-biscuits')
-donuts2.innerHTML = root2.getAttribute('data-donuts')
-pancakes2.innerHTML = root2.getAttribute('data-pancakes')
-status2.innerHTML = root2.getAttribute('data-delivered') === "true" ? 'Delivered' : 'Pending'
-
-biscuits3.innerHTML = root3.getAttribute('data-biscuits')
-donuts3.innerHTML = root3.getAttribute('data-donuts')
-pancakes3.innerHTML = root3.getAttribute('data-pancakes')
-status3.innerHTML = root3.getAttribute('data-delivered') === "true" ? 'Delivered' : 'Pending'
+roots.root3.biscuits.innerHTML = roots.root3.dataKey.getAttribute('data-biscuits')
+roots.root3.donuts.innerHTML = roots.root3.dataKey.getAttribute('data-donuts')
+roots.root3.pancakes.innerHTML = roots.root3.dataKey.getAttribute('data-pancakes')
+roots.root3.status.innerHTML = roots.root3.dataKey.getAttribute('data-delivered') === "true" ? 'Delivered' : 'Pending'
