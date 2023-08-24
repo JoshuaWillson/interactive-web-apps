ac0da2531cfdfab1d67b7a7107a264a97447a739
Joshua Willson <joshuawillsonmj@gmail.com>
2023-08-24 14:21:16 +0200

    commented IWA_12/13 and started IWA_14 challenges

--- IWA13/scripts.js
+++ IWA13/scripts.js
@@ -1,3 +1,54 @@
+// IWA_13 Challenge 1
+
+// BROKEN
+
+// let state = 'idle'
+// let user = null
+// let calculated = '1'
+
+// // Only allowed to change below
+
+
+// const = logCalc () => { 
+//     const isString = typeof calculated = 'numerical-string' 
+//     const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
+//     calculated === calculatedAsNumber + 1 
+// }
+
+// const = calcUser () => {
+//   logCalc
+//   if (calculated > 2) user = 'John'
+//   if (calculated > 2) state = 'requesting'
+//   if (calculated > 3) state = 'idle'
+// }
+
+// const = checkUser () => {
+// 	if (user && state === 'requesting') {
+// 		console.log(`User: ${user} (${calculated})`)
+// 	}
+// }
+
+// // Only allowed to change code above
+
+// checkUser()
+// calcUser()
+
+// checkUser()
+// calcUser()
+
+// checkUser()
+// calcUser()
+
+// checkUser()
+// calcUser()
+
+// checkUser()
+// calcUser()
+
+/***************************************************************************************/
+
+// FIXED
+
 let state = 'idle'
 let user = null
 let calculated = '1'
---
+++ IWA14/scripts.js
@@ -0,0 +1,16 @@
+// IWA_14 Challenge 1
+
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = () => {
+  console(parameter)
+  console(parameter)
+}
+
+function hobby () {
+  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+}
+
+hobby()
